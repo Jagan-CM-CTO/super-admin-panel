00ac2bfb498b51e76504c5ace63093eb96274b3d
Jagan-CM-CTO <118348656+Jagan-CM-CTO@users.noreply.github.com>
2023-06-01 00:08:05 +0530

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,34 +1,57 @@
-import Sidebar from "@/components/Sidebar";
-import Head from "next/head";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useToast, Text, Flex } from "@chakra-ui/react";
-
-import { MdDeleteOutline } from "react-icons/md";
 import axios from "axios";
-import useRouter from "next/router";
-import SellerCard from "../components/SellerCard";
+import { useRouter } from "next/router";
+import { MdDeleteOutline } from "react-icons/md";
+import Head from "next/head";
+
+import Sidebar from "@/components/Sidebar";
+import SellerCard from "@/components/SellerCard";
 import { isAuthenticated } from "@/helper/auth";
 import { API_URL } from "@/helper/api";
 
 const Sellers = () => {
   const [sellers, setSellers] = useState([]);
+  const [profits, setProfits] = useState([]);
   const toast = useToast();
 
-  let auth = isAuthenticated();
-  let jwt = auth.data?.jwt;
-  // console.log(jwt);
-  const getSellers = async () => {
-    let res = await axios.get(`${API_URL}seller-accounts?populate=*`, {
+  const auth = isAuthenticated();
+  const jwt = auth.data?.jwt;
+
+  const getSellerEarnings = async () => {
+    let res = await axios.get(`${API_URL}seller-earnings`, {
       headers: {
         Authorization: `Bearer ${jwt}`,
       },
     });
-    setSellers(res.data.data);
-    // console.log(res.data.data);
+    let data = res.data;
+    setProfits(data);
+  };
+
+  const getSellers = async () => {
+    try {
+      const res = await axios.get(`${API_URL}seller-accounts?populate=*`, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      setSellers(res.data.data);
+      console.log("hurrr", sellers);
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Error",
+        description: "Failed to fetch sellers",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
   };
 
   useEffect(() => {
     getSellers();
+    getSellerEarnings();
   }, []);
 
   return (
@@ -41,28 +64,22 @@ const Sellers = () => {
       </Head>
       <main>
         <Sidebar>
-          <Flex justify={"space-between"}>
-            <Text fontSize={"2xl"} fontWeight={"semibold"}>
+          <Flex justify="space-between">
+            <Text fontSize="2xl" fontWeight="semibold">
               Manage Sellers
             </Text>
           </Flex>
-          <Flex justify={"space-around"} flexWrap={"wrap"}>
-            {sellers.map((seller, i) => {
-              return (
-                <SellerCard
-                  key={i}
-                  sellerName={
-                    seller.attributes.seller.data.attributes.first_name
-                  }
-                  displayName={seller.attributes.display_name}
-                  data={seller}
-                />
-              );
-            })}
-            <SellerCard />
-            <SellerCard />
-            <SellerCard />
-            <SellerCard />
+          <Flex justify="space-around" flexWrap="wrap">
+            {sellers.map((seller, i) => (
+              <SellerCard
+                key={i}
+                sellerName={seller.attributes.seller.data.attributes.first_name}
+                displayName={seller.attributes.display_name}
+                seller={seller}
+                profits={profits}
+                sellerID={seller.id}
+              />
+            ))}
           </Flex>
         </Sidebar>
       </main>
